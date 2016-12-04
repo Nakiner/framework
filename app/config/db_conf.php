ea21953659cf68e11a8f5a5e508d929cb95b3bf3
Pavel Emelyanov <punkbeast553@gmail.com>
2016-12-04 20:29:50 +0300

    Залил фреймворк

---
+++ app/config/db_conf.php
@@ -0,0 +1,10 @@
+<?php
+$config = Dbconnect::instance();
+$config->set(array(
+    'host' => 'localhost',
+    'user' => 'root',
+    'pass' => '',
+    'name' => 'shit'
+));
+$config->connect();
+unset($config);
