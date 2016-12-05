eed1fabd858a93720d8f0c0c97284c6f4d35c654
Pavel Emelyanov <punkbeast553@gmail.com>
2016-12-05 14:31:07 +0300

    Обновил конфиг

--- app/WebRoot/index.php
+++ app/WebRoot/index.php
@@ -11,7 +11,7 @@ define('LOGS_ROOT',APP_PATH.DS.'temp'.DS.'logs');
 define('TEMP',APP_PATH.DS.'temp');
 
 define('TABLE_PREFIX','');
-define('SERVER','http://localhost/');
+define('SERVER','http://corp11.hol.es/');
 ini_set('session.cookie_lifetime', 0);
 
 include_once APP_PATH.DS.'config'.DS.'bootstrap.php';
